fa9dd7bbae87a69f886bece5e125542b1bb0524f
“Richard Middleton” <Richard@richardmiddleton.me>
2017-04-01 00:07:30 +0200

    tidy

--- js/main.js
+++ js/main.js
@@ -1,42 +1,29 @@
-function detectmob() {
-  if (window.innerWidth <= 465) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
 // MODAL
 
-// if (!detectmob()) {
+// Grab from DOM
+var modal = document.querySelector(".modal");
+var modalBg = document.querySelector(".modal-bg");
+var closeButton = document.querySelector(".close-button");
+var closeBg = document.querySelector(".modal-bg");
+var openImg = document.getElementsByClassName("open-modal");
 
-  // Grab from DOM
-  var modal = document.querySelector(".modal");
-  var modalBg = document.querySelector(".modal-bg");
-  var closeButton = document.querySelector(".close-button");
-  var closeBg = document.querySelector(".modal-bg");
-  var openImg = document.getElementsByClassName("open-modal");
+// Event listeners
+closeButton.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalBg.classList.toggle("closed");
+});
 
-  // Event listeners
-  closeButton.addEventListener("click", function () {
+closeBg.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalBg.classList.toggle("closed");
+});
+
+// Iterate through and add event listeners to open-img
+for (var i = 0; i < openImg.length; i++) {
+  openImg[i].addEventListener('click', function (e) {
+    e.preventDefault();
+
     modal.classList.toggle("closed");
     modalBg.classList.toggle("closed");
   });
-
-  closeBg.addEventListener("click", function () {
-    modal.classList.toggle("closed");
-    modalBg.classList.toggle("closed");
-  });
-
-  // Iterate through and add event listeners to open-img
-  for (var i = 0; i < openImg.length; i++) {
-    openImg[i].addEventListener('click', function (e) {
-e.preventDefault();
-
-      modal.classList.toggle("closed");
-      modalBg.classList.toggle("closed");
-    });
-  }
-// }
-
+}
